3932a387d1b231859efa0600b698e07bb510f0bb
dzenvikas <vikaskumar.vsk@gmail.com>
2019-06-09 13:37:50 +0530

    First commit

---
+++ src/modal.js
@@ -0,0 +1,10 @@
+const section = {
+  rowID0: [{ col_ID: "" }, { col_ID: "" }],
+  rowID1: [{ col_ID: "" }, { col_ID: "" }],
+  rowID2: [{ col_ID: "" }, { col_ID: "" }],
+  sorting: {
+    rows: ["rowID1", "rowID2", "rowI0"]
+  }
+};
+
+export default section;
